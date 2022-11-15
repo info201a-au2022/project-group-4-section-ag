adc9daeed562346ea173e3cdc5b7fee387b7f52a
Qinghui Xie <qxie@uw.edu>
2022-11-14 19:35:03 -0800

    rmd testt

--- source/chart_hui.R
+++ source/chart_hui.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 source("./table.R")
 age_versus_inappropriate_content_degree <- ggplot(data = final_table) +
-  geom_col(mapping = aes(x = final_table$age, y = final_table$inappropriate_content_degree), color = "#4cc1cc", fill = "#9428de") +
+  geom_line(mapping = aes(x = final_table$age, y = final_table$inappropriate_content_degree), color = "#4cc1cc", fill = "#9428de") +
   labs(x = "Age", y = "Degree of Severity", title = "Age and the Degree of Severity in Inappropriate Content")
 age_versus_inappropriate_content_degree
