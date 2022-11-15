920f72f3a813d6cb901b3c02612217f8e719efc0
hpia22 <hpia22@uw.edu>
2022-11-14 18:42:27 -0800

    haipei one chart and summary paragraph

--- source/chart_haipei.R
+++ source/chart_haipei.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 source("./table.R")
 age_versus_number_of_sm_plot <- ggplot(data = final_table) +
-  geom_col(mapping = aes(x = final_table$age, y = final_table$number_of_social_media_platforms), color = "#6F859E", fill = "#B2CDEE") +
+  geom_point(mapping = aes(x = final_table$age, y = final_table$number_of_social_media_platforms), color = "#6F859E") +
   labs(x = "Age", y = "Number of Social Media", title = "The Number of Social Media Platforms of Youth Aged 14-23")
 age_versus_number_of_sm_plot
